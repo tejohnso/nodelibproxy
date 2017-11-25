2af55a381e9f1231f50540cea2c1b91aa85cfe9b
Tyler Johnson <mail@tyler-johnson.ca>
2017-11-25 12:55:42 -0500

    Improve tests

--- samples/node/run.sh
+++ samples/node/run.sh
@@ -1,1 +1,1 @@
-HTTP_PROXY=http://192.168.0.1 node test.js
+no_proxy=localhost HTTP_PROXY=http://192.168.1.1 node test.js
--- samples/node/test.js
+++ samples/node/test.js
@@ -1,5 +1,11 @@
 const proxyCheck = require("./build/Release/proxycheck");
-proxyCheck.checkProxyFor("http://www.google.com", (err, proxy)=>{
-  if (err) {return console.log(err);}
-  console.log(proxy);
-});
+const checkProxyFor = require("util").promisify(proxyCheck.checkProxyFor);
+
+checkProxyFor("http://www.google.com")
+.then(console.log);
+
+checkProxyFor("http://localhost")
+.then(console.log);
+
+checkProxyFor()
+.catch(err=>err.message.includes("number of arguments") ? "" : err);
